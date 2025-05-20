78edd91dfbaa415a223e932d31e45c4432d52063
José M. Á. R. R <josrr@ymail.com>
2025-05-20 08:49:50 -0600

    + the missiles disappear when their lifetime expires

--- missiles.lisp
+++ missiles.lisp
@@ -2,7 +2,7 @@
 
 ;;;;
 (defclass missile (explosible obj gravitational toroidal)
-  ((lifetime :initform 120 :reader lifetime))
+  ((lifetime :initform 480 :accessor lifetime))
   (:default-initargs :dx 1.0 :dy 1.0))
 
 (defun make-missile (x y dx dy)
@@ -14,7 +14,10 @@
 (defmethod update ((obj missile) context)
   (let ((*display* (display context)))
     (incf (x obj) (/ (dx obj) 8.0))
-    (incf (y obj) (/ (dy obj) 8.0))))
+    (incf (y obj) (/ (dy obj) 8.0))
+    (if (zerop (lifetime obj))
+        (setf (objects context) (remove obj (objects context)))
+        (decf (lifetime obj)))))
 
-(defmethod draw ((obj missile) context)
-  (display:draw-point (display context) (x obj) (y obj) 1))
+(defmethod draw ((obj missile) display)
+  (display:draw-point display (x obj) (y obj) 1))
